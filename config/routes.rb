bd8384cd7ff14a90a64e9fcd9dd70d025e12de2c
koji.saito <koji_s.0308@keio.jp>
2020-02-09 12:15:55 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   root to: 'tasks#index'
+  
+  get 'signup', to: 'users#new'
 
   resources :tasks
+  
+  resources :users, only: [:new, :create]
 end
